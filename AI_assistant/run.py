bf29d08083fd02788dd2d59398f36a15ede3aa29
Kamguia Stephane <kamguia0000kouam@gmail.com>
2026-01-28 05:31:35 -0500

    Delete AI_assistant directory

--- AI_assistant/run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.logger.warning("Starting SongWish AI Shopping Assistant...")
-    app.run(debug=app.config.get('DEBUG', False), port=5000)
